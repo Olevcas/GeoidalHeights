acb4b5b6ed369b3c6363c4755bdfec32ed52822c
Ole <oleevca@gmail.com>
2024-10-16 13:34:50 +0200

    Dramatically improved runtime by modifying pN

--- Part_1_1/MainCodes/main.py
+++ Part_1_1/MainCodes/main.py
@@ -4,6 +4,8 @@ import sys
 sys.path.append('./Part_1_1/')
 from Constants import constants
 import numba
+from functools import lru_cache
+
 
 
 @numba.njit
@@ -21,45 +23,50 @@ def r_nm(c, n, m):
     else:
         return c
 
-@numba.njit
+@lru_cache(None)
 def pN1(t, n, pn1, pn2):
-    return ((-(np.sqrt(2 * n + 1) / n) * ((n - 1) / (np.sqrt(2 * n - 3))) * pn2) + t * ((np.sqrt(2 * n + 1) / n) * np.sqrt(2 * n - 1) * pn1))
+    return ((-(np.sqrt(2*n+1))/n)*((n-1)/(np.sqrt(2*n-3)))*pn2 + t * ((np.sqrt(2*n+1))/(n))*np.sqrt(2*n-1)*pn1)
 
-@numba.njit
+@lru_cache(None)
 def pN2(t, n, m, pn1, pn2):
-    return (-np.sqrt(((2 * n + 1) * (n + m - 1) * (n - m - 1)) / ((2 * n - 3) * (n + m) * (n - m))) * pn2 + t * np.sqrt(((2 * n + 1) * (2 * n - 1)) / ((n + m) * (n - m))) * pn1)
+    return (-np.sqrt(((2*n+1)*(n+m-1)*(n-m-1))/((2*n-3)*(n+m)*(n-m)))*pn2 + t*np.sqrt(((2*n+1)*(2*n-1))/((n+m)*(n-m)))*pn1)
 
-@numba.njit
+@lru_cache(None)
 def pN3(t, n, pn1):
-    return (t * np.sqrt(2 * n + 1) * pn1)
+    return t * np.sqrt(2*n+1) * pn1
 
-@numba.njit
+@lru_cache(None)
 def pN4(t, n, pn1):
-    return (np.sqrt((2 * n + 1) / (2 * n)) * np.sqrt(1 - t ** 2) * pn1)
+    return np.sqrt((2*n+1)/(2*n)) * np.sqrt(1-t**2) * pn1
 
-@numba.njit
+# Memoize the main function
+@lru_cache(None)
 def pN_main(n, m, latitude):
     t = np.sin(latitude)
-    if n == 0 and m == 0:
-        return 1
-    elif n == 1 and m == 0:
-        return t * np.sqrt(3)
-    elif n == 2 and m == 0:
-        return np.sqrt(5) * (1.5 * t ** 2 - 0.5)
-    elif n == 1 and m == 1:
-        return np.sqrt(3) * np.sqrt(1 - t ** 2)
-    elif n == 2 and m == 1:
-        return t * np.sqrt(15) * np.sqrt(1 - t ** 2)
-    elif n >= 2 and m == 0:
-        return pN1(t, n, pN_main(n - 1, 0, latitude), pN_main(n - 2, 0, latitude))
-    elif n >= 3 and m >= 1 and m <= n - 2:
-        return pN2(t, n, m, pN_main(n - 1, m, latitude), pN_main(n - 2, m, latitude))
-    elif n >= 1 and m == (n - 1):
-        return pN3(t, n, pN_main(n - 1, n - 1, latitude))
-    elif n >= 2 and m == n:
-        return pN4(t, n, pN_main(n - 1, n - 1, latitude))
 
-@numba.njit
+    if (n == 0 and m == 0):
+        return 1
+    elif (n == 1 and m == 0):
+        return t * np.sqrt(3)
+    elif (n == 2 and m == 0):
+        return np.sqrt(5) * ((1.5 * t**2) - (1/2))
+    elif (n == 1 and m == 1):
+        return np.sqrt(3) * np.sqrt(1 - t**2)
+    elif (n == 2 and m == 1):
+        return t * np.sqrt(15) * np.sqrt(1 - t**2)
+
+    elif (n >= 2 and m == 0):
+        return pN1(t, n, pN_main(n-1, 0, latitude), pN_main(n-2, 0, latitude))
+
+    elif (n >= 3 and m >= 1 and m <= n-2):
+        return pN2(t, n, m, pN_main(n-1, m, latitude), pN_main(n-2, m, latitude))
+
+    elif (n >= 1 and m == (n-1)):
+        return pN3(t, n, pN_main(n-1, n-1, latitude))
+
+    elif (n >= 2 and m == n):
+        return pN4(t, n, pN_main(n-1, n-1, latitude))
+
 def compute_geoidal_height(lat_radians, long_radians, R, model_values):
     sum = 0
     constant_term = constants.gm / (R * constants.gamma)
@@ -86,4 +93,4 @@ def geoidalHeight(latitude, longitude, R, model):
     return geoidUndulation
 
 # Call the function
-geoidalHeight(61.9308563192723, 5.12764703841812, constants.r, constants.df_EGM2008)
+geoidalHeight(61.6929259311394, 5.1957949286442, constants.r, constants.df_EGM2008)
