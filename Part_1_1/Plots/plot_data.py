e20dedf0cf60be07768d94c63d940facab5ec436
Ole <oleevca@gmail.com>
2025-01-10 12:43:27 +0100

    Added reqs

--- Part_1_1/Plots/plot_data.py
+++ Part_1_1/Plots/plot_data.py
@@ -5,18 +5,18 @@ sys.path.append('./Part_1_1/')
 from MainCodes import main
 from Constants import constants
 
-
+#Instantiates the dataframes made to contain the data from EGM2008 and GGM03S
 df_plot_data_EGM2008 = pd.DataFrame(columns=['Latitude', 'Longitude', 'Geoidal_height'])
 df_plot_data_GGM03S = pd.DataFrame(columns=['Latitude', 'Longitude', 'Geoidal_height'])
 
-
+#Step size for how often to calculate the geoidal height 
 step_size = 1
 
+#Area of interest
 longitudes = np.arange(-30, 30 + step_size, step_size)
 latitudes = np.arange(45, 75 + step_size, step_size)
 
-# Create meshgrid of latitudes and longitudes.
-
+# Create meshgrid of latitudes and longitudes, and then estimate geoidal heights for all points inside this grid with the chosen step size
 def make_plot_data(latitudes, longitudes):
 
     global df_plot_data_EGM2008
@@ -39,11 +39,12 @@ def make_plot_data(latitudes, longitudes):
     df_plot_data_EGM2008.to_csv('Part_1_1/Plots/Data/plot_data_EGM2008.txt', sep='A', index=False)
     df_plot_data_GGM03S.to_csv('Part_1_1/Plots/Data/plot_data_GGM03S.txt', sep='A', index=False)
 
-make_plot_data(latitudes, longitudes)
+
+#Example of how to generate data used for plotting
+#make_plot_data(latitudes, longitudes)
     
 
-#df8 = pd.read_csv('Part_1_1/Plots/Data/plot_data_GGM03S.txt', encoding='latin1', sep='A')
-#print(df8)
+
 
 
 
